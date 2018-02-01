75f2ceac7d0d91f3a6889aaa75e53a446cc44764
Dantiff <daniel@touchinspiration.com>
2018-02-01 11:15:23 +0300

    initiate the root reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,19 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux'
-import sessions from '../modules/auth/reducer';
 import users from '../modules/users/reducer';
-import clients from '../modules/clients/reducer';
-import { reducer as formReducer } from 'redux-form';
-import { reducer as toastrReducer } from 'react-redux-toastr';
 
 const rootReducer = combineReducers({
   // short hand property names
-  sessions,
   users,
-  clients,
   routing,
-  form: formReducer,
-  toastr: toastrReducer 
 })
 
 export default rootReducer;
